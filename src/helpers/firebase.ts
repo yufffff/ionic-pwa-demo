debdbab156e9faeaed6ddee52511f2ba31b1cec5
yufffff <y.fukai.0325@gmail.com>
2021-11-20 14:10:44 +0900

    fix

--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -7,8 +7,6 @@ import { IMessage } from "../interfaces/message";
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
-const db = firebase.firestore();
-
 export default auth;
 export const authenticateGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -16,33 +14,32 @@ export const authenticateGoogle = () => {
 };
 
 export const isAuth = () => {
-    return new Promise((resolve) => {
-        firebase.auth().onAuthStateChanged((user) => {
+    return new Promise(resolve => {
+        firebase.auth().onAuthStateChanged(user => {
             resolve(user || null);
         });
     });
 };
 
+const db = firebase.firestore();
+
 export const getUserRef = (userId: string) => {
-    return db.collection('users').doc(userId);
+    return db.collection(`users`).doc(userId);
 };
 
 export const addMessage = (userId: string, message: IMessage) => {
     const ref = getUserRef(userId);
-    return ref.collection('messages').add(message);
-}
+    return ref.collection(`messages`).add(message);
+};
 
 export const getMessages = (userId: string, createdAt: Date = new Date()) => {
     const ref = getUserRef(userId);
-    return ref.collection('messages')
-        .where('createdAt', '<', createdAt)
-        .orderBy('createdAt', 'desc').limit(10).get();
+    return ref.collection('messages').where('createdAt', '<', createdAt).orderBy('createdAt', 'desc').limit(10).get();
 };
 
 export const isTimestamp = (arg: any) => {
     return arg.seconds !== undefined;
 };
-
 export const formatDate = (timeStamp: any) => {
     let dt = new Date();
     if (isTimestamp(timeStamp)) {
@@ -50,27 +47,26 @@ export const formatDate = (timeStamp: any) => {
     }
     return dt.getMonth() + 1 + '/' + dt.getDate();
 };
-
 export const getMessageRef = (userId: string, messageId: string) => {
     const ref = getUserRef(userId);
-    return ref.collection('messages').doc(messageId);
+    return ref.collection(`messages`).doc(messageId);
 };
 
 export const getMessage = (userId: string, messageId: string) => {
     const ref = getMessageRef(userId, messageId);
     return ref.get();
-}
+};
 
 export const getUser = (userId: string) => {
     const ref = getUserRef(userId);
     return ref.get();
-}
+};
 
 export const putMessage = (userId: string, messageId: string, message: IMessage) => {
     const ref = getMessageRef(userId, messageId);
     message.updatedAt = new Date();
     return ref.update(message);
-}
+};
 
 export const getUsers = (createdAt: Date = new Date()) => {
     return db.collection('users').where('createdAt', '<', createdAt).orderBy('createdAt', 'desc').limit(10).get();
@@ -78,7 +74,7 @@ export const getUsers = (createdAt: Date = new Date()) => {
 
 export const signOut = async () => {
     await firebase.auth().signOut();
-}
+};
 
 export const updateUserName = async (userId: string, name: string) => {
     await firebase.auth().currentUser.updateProfile({
@@ -86,4 +82,4 @@ export const updateUserName = async (userId: string, name: string) => {
     });
     const ref = getUserRef(userId);
     return ref.update({ name: name });
-}
+};
